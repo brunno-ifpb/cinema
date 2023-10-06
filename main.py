da6a107a5fe06a393618cb83e5af70ab17fb05c2
Brunno Oliveira Santos Lima <202310040007@ad.ifpb.edu.br>
2023-10-06 10:57:47 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+def caderias():
+    # sistema que pega a situacão do objeto 'cadeira' do dataset
+    # 1 = cadeira ocupada
+    # -1 = cadeira quebrada
+    # 0 = caderira livre
+    pass
+
+def marca_cadeira():
+    # si caderia selecionada verificar se ela ainda esta = 0
+    # se sim, marca a cadeira como 1 no dataset
+    # se não, voltar a tela de escolha
+    pass
+
+def tela_cadeiras():
+    # tela de front-and das cadeiras
+    # si cadeira = 1 cadeira vermelha
+    # si cadeira = -1 cadeira preta com um "X"
+    # si cadeira = 0 cadeira verde
+    pass
+
+def selecionar_cadeira():
+    pass
